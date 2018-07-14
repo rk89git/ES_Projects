b48bfb8db634b1d467ccd0ef2704463f11722546
Rakesh Kumar <kumaar079@gmail.com>
2018-07-14 14:00:39 +0530

    Updated Repository
    Changes

--- src/main/java/com/db/cricket/predictnwin/constants/PredictAndWinIndexes.java
+++
@@ -1,9 +0,0 @@
-package com.db.cricket.predictnwin.constants;
-
-public class PredictAndWinIndexes {
-
-	public static final String USER_WINNERS_LIST = "user_winners_list";
-
-	public static final String USERS_BIDS = "users_bids";
-
-}
